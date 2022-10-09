87ff2b44fcae04ec78c6b58c3ee6a2289347ebfd
Simon <simon.bela.bumiller@gmx.de>
2022-10-09 21:12:51 +0200

    Add DeleteDreamUseCase.kt

---
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/usecase/DeleteDreamUseCase.kt
@@ -0,0 +1,12 @@
+package com.snow.dreamdiary.feature_dreams.domain.usecase;
+
+import com.snow.dreamdiary.feature_dreams.domain.model.Dream
+import com.snow.dreamdiary.feature_dreams.domain.repository.DreamRepository
+
+public class DeleteDreamUseCase(
+    private val repository: DreamRepository
+) {
+    suspend operator fun invoke(dream: Dream){
+        repository.deleteDream(dream)
+    }
+}
