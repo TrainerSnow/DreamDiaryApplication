7329a6f954d4da55402f2f156c1a18bdd488ce41
Simon <simon.bela.bumiller@gmx.de>
2022-10-10 18:02:58 +0200

    Basic structure for Add Dream Screen

--- app/src/main/java/com/snow/dreamdiary/common/presentation/components/TopAppBars.kt
+++
@@ -1,2 +0,0 @@
-package com.snow.dreamdiary.common.presentation.components
-
--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/addeditdream/AddEditDreamScreen.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/addeditdream/AddEditDreamScreen.kt
@@ -1,11 +1,24 @@
 package com.snow.dreamdiary.feature_dreams.presentation.addeditdream;
 
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.Add
+import androidx.compose.material.icons.sharp.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.snow.dreamdiary.R
+import com.snow.dreamdiary.ui.theme.DreamDiaryApplicationTheme
 
 @Composable
 fun AddEditDreamScreen(
@@ -28,19 +41,69 @@ fun AddEditDreamScreen(
 
     val scope = rememberCoroutineScope()
 
+    DreamDiaryApplicationTheme {
 
-    DefaultContent()
+    }
+
 
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DefaultContent() {
-
-}
-
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun DefaultPreviewAddEditDreamScreen() {
-    DefaultContent()
+    DreamDiaryApplicationTheme(
+        darkTheme = true
+    ) {
+        Scaffold(
+            topBar = {
+                SmallTopAppBar(
+                    navigationIcon = {
+                        Icon(
+                            imageVector = Icons.Sharp.ArrowBack,
+                            contentDescription = stringResource(id = R.string.cd_back)
+                        )
+                    },
+                    title = {
+                        Text(
+                            text = stringResource(id = R.string.add_dream),
+                        )
+                    },
+                    colors = TopAppBarDefaults.smallTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer
+                    )
+                )
+            },
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    onClick = { /*TODO*/ },
+                    text = {
+                        Text(
+                            text = stringResource(id = R.string.add)
+                        )
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Sharp.Add,
+                            contentDescription = stringResource(id = R.string.cd_add)
+                        )
+                    }
+                )
+            },
+            floatingActionButtonPosition = FabPosition.End,
+
+            ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Column(
+
+                ) {
+                }
+            }
+        }
+    }
+
 }
