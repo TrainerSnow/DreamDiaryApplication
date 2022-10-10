4432bc67e8f55265cae94ce6cc5f30c39c84dc0f
Simon <simon.bela.bumiller@gmx.de>
2022-10-10 15:12:11 +0200

    Refactor AddEditDreamEvent.kt to new, expanded selection of events

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/addeditdream/AddEditDreamEvent.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/addeditdream/AddEditDreamEvent.kt
@@ -4,5 +4,16 @@ import com.snow.dreamdiary.feature_dreams.domain.model.Dream
 
 sealed class AddEditDreamEvent{
     data class Add(val dream: Dream): AddEditDreamEvent()
+    data class ChangeDescription(val value: String): AddEditDreamEvent()
+    data class ChaAnnotation(val value: String): AddEditDreamEvent()
+
+    data class ChangePersons(val value: List<String>): AddEditDreamEvent()
+    data class ChangeFeelings(val value: List<String>): AddEditDreamEvent()
+    data class ChangeLocations(val value: List<String>): AddEditDreamEvent()
+
+    data class ChangeComfortness(val value: Int): AddEditDreamEvent()
+
+    data class ChangeDreamtAt(val value: Long): AddEditDreamEvent()
+
     object Cancel: AddEditDreamEvent()
 }
