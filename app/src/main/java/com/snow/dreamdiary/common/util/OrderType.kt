bf6b05f7c5c203512073be7175c45d75e63cd8c9
Simon <simon.bela.bumiller@gmx.de>
2022-10-12 13:23:55 +0200

    Add names to order components

--- app/src/main/java/com/snow/dreamdiary/common/util/OrderType.kt
+++ app/src/main/java/com/snow/dreamdiary/common/util/OrderType.kt
@@ -1,6 +1,9 @@
 package com.snow.dreamdiary.common.util
 
-sealed class OrderType {
-    object Ascending : OrderType()
-    object Descending : OrderType()
+import androidx.annotation.StringRes
+import com.snow.dreamdiary.R
+
+sealed class OrderType(@StringRes val name: Int) {
+    object Ascending : OrderType(R.string.order_ascending)
+    object Descending : OrderType(R.string.order_descending)
 }
--- app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamOrder.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamOrder.kt
@@ -1,8 +1,10 @@
 package com.snow.dreamdiary.feature_dreams.domain.util
 
+import androidx.annotation.StringRes
+import com.snow.dreamdiary.R
 import com.snow.dreamdiary.common.util.OrderType
 
-sealed class DreamOrder(val orderType: OrderType) {
-    class Dreamed(orderType: OrderType) : DreamOrder(orderType)
-    class Created(orderType: OrderType) : DreamOrder(orderType)
+sealed class DreamOrder(val orderType: OrderType, @StringRes val name: Int) {
+    class Dreamed(orderType: OrderType) : DreamOrder(orderType, R.string.order_dreamed)
+    class Created(orderType: OrderType) : DreamOrder(orderType, R.string.order_created)
 }
