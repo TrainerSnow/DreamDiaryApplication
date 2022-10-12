8a1b4b5ca2228834dd8356fa011126cb66badf05
Simon <simon.bela.bumiller@gmx.de>
2022-10-12 07:31:01 +0200

    Add state class for viewwing dreams

---
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/viewdreams/ViewDreamsState.kt
@@ -0,0 +1,11 @@
+package com.snow.dreamdiary.feature_dreams.presentation.viewdreams
+
+import com.snow.dreamdiary.common.util.OrderType
+import com.snow.dreamdiary.feature_dreams.domain.model.Dream
+import com.snow.dreamdiary.feature_dreams.domain.util.DreamOrder
+
+data class ViewDreamsState(
+    val dreams: List<Dream> = emptyList(),
+    val sortingOrder: DreamOrder = DreamOrder.Dreamed(OrderType.Descending),
+    val isOrderMenuExpanded: Boolean = false
+)
