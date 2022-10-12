2af198503f13c52b32278fd723df1fa3469dabbb
Simon <simon.bela.bumiller@gmx.de>
2022-10-12 20:26:48 +0200

    Add OrderSection.kt composable

---
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/viewdreams/components/LabeledRadioButton.kt
@@ -0,0 +1,33 @@
+package com.snow.dreamdiary.feature_dreams.presentation.viewdreams.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LabeledRadioButton(
+    onClick: () -> Unit,
+    text: String,
+    selected: Boolean
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(
+            selected = selected,
+            onClick = onClick
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.titleSmall
+        )
+    }
+}
---
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/viewdreams/components/OrderSection.kt
@@ -0,0 +1,69 @@
+package com.snow.dreamdiary.feature_dreams.presentation.viewdreams.components;
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.snow.dreamdiary.R
+import com.snow.dreamdiary.common.util.OrderType
+import com.snow.dreamdiary.feature_dreams.domain.util.DreamOrder
+
+@Composable
+fun OrderSection(
+    order: DreamOrder,
+    onOrderChange: (DreamOrder) -> Unit
+) {
+    Column {
+        Row {
+            LabeledRadioButton(
+                onClick = {
+                    onOrderChange(DreamOrder.Created(order.orderType))
+                },
+                text = stringResource(id = R.string.order_created),
+                selected = order is DreamOrder.Created
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            LabeledRadioButton(
+                onClick = {
+                    onOrderChange(DreamOrder.Dreamed(order.orderType))
+                },
+                text = stringResource(id = R.string.order_dreamed),
+                selected = order is DreamOrder.Dreamed
+            )
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Row {
+            LabeledRadioButton(
+                onClick = {
+                    onOrderChange(
+                        if (order is DreamOrder.Dreamed){
+                            DreamOrder.Dreamed(OrderType.Descending)
+                        }else{
+                            DreamOrder.Created(OrderType.Descending)
+                        }
+                    )
+                },
+                text = stringResource(id = R.string.order_descending),
+                selected = order.orderType == OrderType.Descending
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            LabeledRadioButton(
+                onClick = {
+                    onOrderChange(
+                        if (order is DreamOrder.Dreamed){
+                            DreamOrder.Dreamed(OrderType.Ascending)
+                        }else{
+                            DreamOrder.Created(OrderType.Ascending)
+                        }
+                    )
+                },
+                text = stringResource(id = R.string.order_ascending),
+                selected = order.orderType == OrderType.Ascending
+            )
+        }
+
+    }
+}
