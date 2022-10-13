e0d7dbc6af92a54af950880d71062db289841f36
Simon <simon.bela.bumiller@gmx.de>
2022-10-13 18:37:19 +0200

    Center "You dont have any dreams" text

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/viewdreams/ViewDreamsScreen.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/viewdreams/ViewDreamsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -112,7 +113,8 @@ fun ViewDreamsScreen(
                 }else{
                     Box(
                         modifier = Modifier
-                            .background(Color.Transparent),
+                            .background(Color.Transparent)
+                            .fillMaxSize(),
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
