48b648e9a1ccdccd34c2f1b79767cc5df67b5bd2
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 10:58:36 +0200

    Add probably only event thats going to happen

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/search/selectmode/SelectSearchModeEvent.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/search/selectmode/SelectSearchModeEvent.kt
@@ -1,5 +1,7 @@
 package com.snow.dreamdiary.feature_dreams.presentation.search.selectmode
 
-sealed class SelectSearchModeEvent{
+import com.snow.dreamdiary.feature_dreams.domain.util.DreamSearchModes
 
+sealed class SelectSearchModeEvent{
+    data class ModeSelected(val mode: DreamSearchModes): SelectSearchModeEvent()
 }
