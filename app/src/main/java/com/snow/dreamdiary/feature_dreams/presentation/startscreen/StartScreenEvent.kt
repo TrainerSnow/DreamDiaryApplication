5f349fba8dbf8b1f3b5cc2ba2b1317643e5bf6ec
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 11:02:56 +0200

    Add event for opening screen

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/startscreen/StartScreenEvent.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/startscreen/StartScreenEvent.kt
@@ -1,5 +1,7 @@
 package com.snow.dreamdiary.feature_dreams.presentation.startscreen
 
-sealed class StartScreenEvent{
+import com.snow.dreamdiary.feature_dreams.presentation.navigation.DreamScreens
 
+sealed class StartScreenEvent{
+    data class OpenScreen(val screen: DreamScreens): StartScreenEvent()
 }
