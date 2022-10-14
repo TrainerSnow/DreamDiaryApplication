83447155b17882ec40728b473399914b52e6fcfe
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 12:07:43 +0200

    Add empty nav graph for options

---
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/navigation/StartNavGraphDreams.kt
@@ -0,0 +1,18 @@
+package com.snow.dreamdiary.feature_dreams.presentation.navigation;
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.snow.dreamdiary.feature_dreams.presentation.addeditdream.AddEditDreamScreen
+import com.snow.dreamdiary.feature_dreams.presentation.startscreen.DreamStartScreen
+import com.snow.dreamdiary.feature_dreams.presentation.viewdreams.ViewDreamsScreen
+
+@Composable
+fun StartNavGraphDreams(
+    navController: NavHostController
+) {
+
+}
