b0c7fdad28ac605c86148616817bac5a9c79bbf2
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 12:19:06 +0200

    Add example Buttons, to be changed later

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewScreen.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewScreen.kt
@@ -1,22 +1,45 @@
 package com.snow.dreamdiary.feature_dreams.presentation.optionsoverview
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import com.snow.dreamdiary.R
 
 @Composable
 fun OptionsOverviewScreen(
     navController: NavHostController
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier =
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "OptionsOverviewScreen")
+        // TODO: Change this from button to good looking squares
+
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(
+                text = stringResource(id = R.string.search_dreams)
+            )
+        }
+
+        for( i in 0 until 4){
+            Button(
+                onClick = { /*TODO*/ }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.search_dreams)
+                )
+            }
+        }
     }
 }
