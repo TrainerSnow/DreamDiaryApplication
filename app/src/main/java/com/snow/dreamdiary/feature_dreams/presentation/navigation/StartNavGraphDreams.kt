13bf4497ebfcfc3a675bbcd0d8611a4177fefd3c
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 12:09:41 +0200

    Add start graph

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/navigation/StartNavGraphDreams.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/navigation/StartNavGraphDreams.kt
@@ -2,17 +2,28 @@ package com.snow.dreamdiary.feature_dreams.presentation.navigation;
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import com.snow.dreamdiary.feature_dreams.presentation.addeditdream.AddEditDreamScreen
-import com.snow.dreamdiary.feature_dreams.presentation.startscreen.DreamStartScreen
-import com.snow.dreamdiary.feature_dreams.presentation.viewdreams.ViewDreamsScreen
+import com.snow.dreamdiary.feature_dreams.presentation.optionsoverview.OptionsOverviewScreen
+import com.snow.dreamdiary.feature_dreams.presentation.searchmode.SearchModeScreen
 
 @Composable
 fun StartNavGraphDreams(
     navController: NavHostController
 ) {
-
+    NavHost(
+        navController = navController,
+        startDestination = DreamScreens.DreamStartScreen.route
+    ) {
+        composable(
+            route = DreamScreens.SearchModeScreen.route
+        ) {
+            SearchModeScreen(navController = navController)
+        }
+        composable(
+            route = DreamScreens.OptionsOverviewScreen.route
+        ) {
+            OptionsOverviewScreen(navController = navController)
+        }
+    }
 }
