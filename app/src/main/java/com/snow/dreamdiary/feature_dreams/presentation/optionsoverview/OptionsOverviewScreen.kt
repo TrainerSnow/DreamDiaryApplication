31485ead7f74d7b2ee97f148d56eba74cf33c6a6
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 22:11:55 +0200

    Design options menu when started

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewScreen.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewScreen.kt
@@ -2,18 +2,27 @@ package com.snow.dreamdiary.feature_dreams.presentation.optionsoverview
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.snow.dreamdiary.R
 import com.snow.dreamdiary.feature_dreams.presentation.navigation.DreamScreens
+import com.snow.dreamdiary.feature_dreams.presentation.optionsoverview.components.OptionsItem
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -31,27 +40,31 @@ fun OptionsOverviewScreen(
     }
 
     Column(
-        modifier =
-        Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        // TODO: Change this from button to good looking squares
 
-        Button(
-            onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.SearchModeScreen)) }
-        ) {
-            Text(
-                text = stringResource(id = R.string.search_dreams)
-            )
-        }
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalArrangement = Arrangement.spacedBy(24.dp),
+            contentPadding = PaddingValues(horizontal = 24.dp, vertical = 24.dp)
+        ){
+            item {
+                OptionsItem(
+                    text = stringResource(id = R.string.search_dreams),
+                    icon = Icons.Rounded.Search,
+                    onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.SearchModeScreen)) },
+                    num = 0
+                )
+            }
 
-        for( i in 0 until 4){
-            Button(
-                onClick = { /*TODO*/ }
-            ) {
-                Text(
-                    text = stringResource(id = R.string.search_dreams)
+            items(count = 4, key = null){ i ->
+                OptionsItem(
+                    text = "Item nr $i",
+                    icon = Icons.Rounded.Search,
+                    onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.SearchModeScreen)) },
+                    num = i + 1
                 )
             }
         }
