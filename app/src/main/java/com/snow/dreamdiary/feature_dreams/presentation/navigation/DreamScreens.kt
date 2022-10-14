18f05d379cef11f73f4a4f49b7d56da09181fe2b
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 20:34:26 +0200

    Add new Screens

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/navigation/DreamScreens.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/navigation/DreamScreens.kt
@@ -3,7 +3,7 @@ package com.snow.dreamdiary.feature_dreams.presentation.navigation
 import com.snow.dreamdiary.feature_dreams.domain.util.DreamSearchModes
 
 sealed class DreamScreens(
-    var route: String
+    open var route: String
 ) {
 
     object SearchModeScreen : DreamScreens(
@@ -14,6 +14,28 @@ sealed class DreamScreens(
         route = "route_optionsoverviewscreen"
     )
 
+    data class SearchDreamtScreen(override var route: String = "route_searchdreamtscreen/{$KEY_MODE}"): DreamScreens(route){
+        fun withMode(mode: DreamSearchModes.ByDreamt): SearchDreamtScreen{
+            this.route = this.route.replace("{$KEY_MODE}", mode.toJson().toString())
+            return this
+        }
+    }
+
+    data class SearchModifierScreen(override var route: String = "route_searchmodifierscreen/{$KEY_MODE}"): DreamScreens(route){
+        fun withMode(mode: DreamSearchModes.ByModifier): SearchModifierScreen{
+            this.route = this.route.replace("{$KEY_MODE}", mode.toJson().toString())
+            return this
+        }
+    }
+
+    data class SearchComfortnessScreen(override var route: String = "route_searchcomfortnessscreen/{$KEY_MODE}"): DreamScreens(route){
+        fun withMode(mode: DreamSearchModes.ByComfortness): SearchComfortnessScreen{
+            this.route = this.route.replace("{$KEY_MODE}", mode.toJson().toString())
+            return this
+        }
+    }
+
+    @Deprecated("Don't use, won't work")
     object SearchConfigScreen : DreamScreens(
         route = "route_searchconfigscreen/{mode}"
     ){
