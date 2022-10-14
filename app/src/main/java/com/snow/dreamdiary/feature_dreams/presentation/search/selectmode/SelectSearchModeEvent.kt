b9c6de1b55fbbb18ca0fc55198abff64f4cbe0b2
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 11:58:51 +0200

    delete old files

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/search/selectmode/SelectSearchModeEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.snow.dreamdiary.feature_dreams.presentation.search.selectmode
-
-import com.snow.dreamdiary.feature_dreams.domain.util.DreamSearchModes
-
-sealed class SelectSearchModeEvent{
-    data class ModeSelected(val mode: DreamSearchModes): SelectSearchModeEvent()
-}
--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/search/selectmode/SelectSearchModeState.kt
+++
@@ -1,3 +0,0 @@
-package com.snow.dreamdiary.feature_dreams.presentation.search.selectmode
-
-data class SelectSearchModeState(val n: Int = 0)
