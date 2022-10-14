cf343c1071b66ee8bb74e5c5347fcfa204c224b2
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 11:07:19 +0200

    Handle open new screen (will be evoked on button click)

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/startscreen/StartScreenViewModel.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/startscreen/StartScreenViewModel.kt
@@ -1,10 +1,33 @@
 package com.snow.dreamdiary.feature_dreams.presentation.startscreen;
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.snow.dreamdiary.feature_dreams.presentation.navigation.DreamScreens
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 public class StartScreenViewModel(
 
-) {
+): ViewModel() {
 
+
+    private val _actionFlow = MutableSharedFlow<UIEvent>()
+    val actionFlow = _actionFlow.asSharedFlow()
+
+    fun onEvent(event: StartScreenEvent){
+        when (event) {
+            is StartScreenEvent.OpenScreen -> {
+                viewModelScope.launch {
+                    _actionFlow.emit(UIEvent.OpenScreen(event.screen))
+                }
+            }
+        }
+    }
+
+    sealed class UIEvent{
+        data class OpenScreen(val screen: DreamScreens): UIEvent()
+    }
 }
