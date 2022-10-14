3f3306a1960faea3474c624b846facf452e2f381
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 20:29:33 +0200

    Add empty files for modifier search config

---
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/searchconfig/bymodifier/SearchModifierScreen.kt
@@ -0,0 +1,12 @@
+package com.snow.dreamdiary.feature_dreams.presentation.searchconfig.bymodifier
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.snow.dreamdiary.feature_dreams.presentation.searchconfig.bydreamt.SearchDreamtViewModel
+
+@Composable
+fun SearchModifierScreen(
+    viewModel: SearchDreamtViewModel = hiltViewModel()
+) {
+
+}
---
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/searchconfig/bymodifier/SearchModifierState.kt
@@ -0,0 +1,3 @@
+package com.snow.dreamdiary.feature_dreams.presentation.searchconfig.bymodifier
+
+data class SearchModifierState(val n: Int = 0)
