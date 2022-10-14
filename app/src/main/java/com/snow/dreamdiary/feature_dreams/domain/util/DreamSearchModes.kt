39dd780032d3b51af611ea6f0901649b0c018dac
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 16:53:58 +0200

    Add dfeault values to dream search modes

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamSearchModes.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamSearchModes.kt
@@ -5,15 +5,15 @@ import com.snow.dreamdiary.common.util.LogicGate
 sealed class DreamSearchModes{
     data class ByModifier(
         val modifier: DreamModifier = DreamModifier.Person,
-        val values: List<String>,
+        val values: List<String> = listOf(""),
         val gate: LogicGate = LogicGate.And
     ): DreamSearchModes()
     data class ByComfortness(
-        val fromVal: Byte,
-        val toVal: Byte
+        val fromVal: Byte = 0,
+        val toVal: Byte = 0
     ): DreamSearchModes()
     data class ByDreamt(
-        val fromTime: Long,
-        val toTime: Long
+        val fromTime: Long = 0,
+        val toTime: Long = 0
     ): DreamSearchModes()
 }
