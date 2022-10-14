7b616560d14798dba66290ed3a3873ce75c4c864
Simon <simon.bela.bumiller@gmx.de>
2022-10-14 16:52:36 +0200

    Add LogicGate to dream search mode

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamSearchModes.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamSearchModes.kt
@@ -1,9 +1,12 @@
 package com.snow.dreamdiary.feature_dreams.domain.util
 
+import com.snow.dreamdiary.common.util.LogicGate
+
 sealed class DreamSearchModes{
     data class ByModifier(
-        val modifiers: DreamModifier,
-        val values: List<String>
+        val modifier: DreamModifier = DreamModifier.Person,
+        val values: List<String>,
+        val gate: LogicGate = LogicGate.And
     ): DreamSearchModes()
     data class ByComfortness(
         val fromVal: Byte,
