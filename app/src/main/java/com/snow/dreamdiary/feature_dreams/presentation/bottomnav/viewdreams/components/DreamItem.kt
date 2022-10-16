a8f825c72ef9d6b0a41e46dcaec0199520aff027
Simon <simon.bela.bumiller@gmx.de>
2022-10-16 12:47:49 +0200

    Add comfrtoness to DreamItem.kt

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/bottomnav/viewdreams/components/DreamItem.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/bottomnav/viewdreams/components/DreamItem.kt
@@ -11,8 +11,10 @@ import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.snow.dreamdiary.R
 import com.snow.dreamdiary.feature_dreams.domain.model.Dream
@@ -89,6 +91,20 @@ fun DreamItem(
                     DreamModifierSection(
                         name = stringResource(id = R.string.locations),
                         items = dream.locations
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.comfortness_x),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = dream.comfortness.toString(),
+                        style = MaterialTheme.typography.titleLarge,
                     )
                 }
 
