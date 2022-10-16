e3c621aaeea4bcaae4543e4d6e6b263aec6fff74
Simon <simon.bela.bumiller@gmx.de>
2022-10-16 17:21:00 +0200

    Revert "sdfsfdsdf"
    This reverts commit d4f9d6352a5c4dffcfbc9e5f7c44517b412dcf38.

--- app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/domain/model/DailySurveyData.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/domain/model/DailySurveyData.kt
@@ -8,7 +8,7 @@ import androidx.room.PrimaryKey
 data class DailySurveyData(
     val didDream: Boolean = false,
     val dreamsNum: Int = -1,
-    val timeSlept: Int = 8,
+    val timeSlept: Long = 8,
     @IntRange(from = 0, to = 10) val health: Int = 5,
     @IntRange(from = 0, to = 10) val physicalActivity: Int = 5,
     @IntRange(from = 0, to = 10) val comfortness: Int = 5,
--- app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/presentation/dailysurvey/DailySurveyScreen.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/presentation/dailysurvey/DailySurveyScreen.kt
@@ -2,14 +2,11 @@ package com.snow.dreamdiary.feature_dailysurvey.presentation.dailysurvey
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Send
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -22,114 +19,97 @@ fun DailySurveyScreen(
     navController: NavHostController
 ) {
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = { viewModel.onEvent(DailySurveyEvent.Send) }) {
-                Icon(
-                    imageVector = Icons.Rounded.Send, 
-                    contentDescription = stringResource(id = R.string.send)
-                )
-            }
-        }
+    Column(
+        modifier = Modifier
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+
     ) {
-        Column(
-            modifier = Modifier
-                .padding(
-                    end = it.calculateEndPadding(LayoutDirection.Ltr) + 16.dp,
-                    start = it.calculateStartPadding(LayoutDirection.Ltr) + 16.dp,
-                    bottom = it.calculateBottomPadding() + 16.dp,
-                    top = it.calculateTopPadding() + 16.dp
-                ),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+        Text(
+            text = stringResource(id = R.string.did_dream_today),
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        )
+        Switch(
+            onCheckedChange = { viewModel.onEvent(DailySurveyEvent.ChangeDidDream(it)) },
+            checked = viewModel.state.value.surveyData.didDream
+        )
 
-        ) {
-            Text(
-                text = stringResource(id = R.string.did_dream_today),
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        //Did dream?
+        Text(
+            text = stringResource(id = R.string.how_many_dreams),
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        )
+        TextField(
+            value = viewModel.state.value.dreamsNum,
+            onValueChange = { text ->
+                viewModel.onEvent(DailySurveyEvent.ChangeDreamsNum(text))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
             )
-            Switch(
-                onCheckedChange = { viewModel.onEvent(DailySurveyEvent.ChangeDidDream(it)) },
-                checked = viewModel.state.value.surveyData.didDream
-            )
-
-            //Did dream?
-            Text(
-                text = stringResource(id = R.string.how_many_dreams),
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
-            )
-            TextField(
-                value = viewModel.state.value.dreamsNum,
-                onValueChange = { text ->
-                    viewModel.onEvent(DailySurveyEvent.ChangeDreamsNum(text))
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number
-                )
-            )
+        )
 
 
-            //Time slept
-            Text(
-                text = stringResource(id = R.string.how_much_slept),
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        //Time slept
+        Text(
+            text = stringResource(id = R.string.how_much_slept),
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        )
+        TextField(
+            value = viewModel.state.value.timeSlept,
+            onValueChange = { text ->
+                viewModel.onEvent(DailySurveyEvent.ChangeTimeSlept(text))
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
             )
-            TextField(
-                value = viewModel.state.value.timeSlept,
-                onValueChange = { text ->
-                    viewModel.onEvent(DailySurveyEvent.ChangeTimeSlept(text))
-                },
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Number
-                )
-            )
+        )
 
 
-            //Health
-            Text(
-                text = stringResource(id = R.string.how_health),
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
-            )
-            Slider(
-                value = viewModel.state.value.surveyData.health.toFloat(),
-                onValueChange = {
-                    viewModel.onEvent(DailySurveyEvent.ChangeHealth(it.toInt()))
-                },
-                valueRange = 0F..10F,
-            )
+        //Health
+        Text(
+            text = stringResource(id = R.string.how_health),
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        )
+        Slider(
+            value = viewModel.state.value.surveyData.health.toFloat(),
+            onValueChange = {
+                viewModel.onEvent(DailySurveyEvent.ChangeHealth(it.toInt()))
+            },
+            valueRange = 0F..10F,
+        )
 
 
-            //Comfortness
-            Text(
-                text = stringResource(id = R.string.how_comfortness),
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
-            )
-            Slider(
-                value = viewModel.state.value.surveyData.comfortness.toFloat(),
-                onValueChange = {
-                    viewModel.onEvent(DailySurveyEvent.ChangeComfortness(it.toInt()))
-                },
-                valueRange = 0F..10F,
-            )
+        //Comfortness
+        Text(
+            text = stringResource(id = R.string.how_comfortness),
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        )
+        Slider(
+            value = viewModel.state.value.surveyData.comfortness.toFloat(),
+            onValueChange = {
+                viewModel.onEvent(DailySurveyEvent.ChangeComfortness(it.toInt()))
+            },
+            valueRange = 0F..10F,
+        )
 
-            //Physical Activity
-            Text(
-                text = stringResource(id = R.string.how_activity),
-                style = MaterialTheme.typography.titleMedium,
-                fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
-            )
-            Slider(
-                value = viewModel.state.value.surveyData.physicalActivity.toFloat(),
-                onValueChange = {
-                    viewModel.onEvent(DailySurveyEvent.ChangeActivity(it.toInt()))
-                },
-                valueRange = 0F..10F,
-            )
-        }
+        //Physical Activity
+        Text(
+            text = stringResource(id = R.string.how_activity),
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = MaterialTheme.typography.bodySmall.fontWeight,
+        )
+        Slider(
+            value = viewModel.state.value.surveyData.physicalActivity.toFloat(),
+            onValueChange = {
+                viewModel.onEvent(DailySurveyEvent.ChangeActivity(it.toInt()))
+            },
+            valueRange = 0F..10F,
+        )
     }
 }
