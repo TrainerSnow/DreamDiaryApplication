302fd9c641f8b538bd67ace2503198de14072621
Simon <simon.bela.bumiller@gmx.de>
2022-10-16 15:29:47 +0200

    Add default values to DailySurveyData.kt

--- app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/domain/model/DailySurveyData.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/domain/model/DailySurveyData.kt
@@ -6,11 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity
 data class DailySurveyData(
-    val didDream: Boolean,
-    val timeSlept: Long,
-    @IntRange(from = 0, to = 10) val health: Int,
-    @IntRange(from = 0, to = 10) val physicalActivity: Int,
-    @IntRange(from = 0, to = 10) val comfortness: Int,
-    @PrimaryKey val idn: Int,
+    val didDream: Boolean = false,
+    val timeSlept: Long = 8 * 60 * 60 * 1000,
+    @IntRange(from = 0, to = 10) val health: Int = 5,
+    @IntRange(from = 0, to = 10) val physicalActivity: Int = 5,
+    @IntRange(from = 0, to = 10) val comfortness: Int = 5,
+    @PrimaryKey val idn: Int? = null,
     val createdAt: Long
 )
