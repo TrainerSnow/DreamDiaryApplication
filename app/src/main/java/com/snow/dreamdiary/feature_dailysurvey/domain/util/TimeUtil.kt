e66b30cd4fa66b393687254539f48307a4c9f2a5
Simon <simon.bela.bumiller@gmx.de>
2022-10-21 18:04:52 +0200

    Unsuewd fun

--- app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/domain/util/TimeUtil.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dailysurvey/domain/util/TimeUtil.kt
@@ -1,7 +1,6 @@
 package com.snow.dreamdiary.feature_dailysurvey.domain.util
 
 import org.joda.time.DateTime
-import java.util.*
 
 object TimeUtil {
 
@@ -9,15 +8,5 @@ object TimeUtil {
         val now = DateTime()
         val today = now.toLocalDate()
         return today.toDateTimeAtStartOfDay().millis
-    }
-
-    private fun millisSinceStartOfToday(): Long {
-        val date: Calendar = GregorianCalendar()
-        // reset hour, minutes, seconds and millis
-        date.set(Calendar.HOUR_OF_DAY, 0)
-        date.set(Calendar.MINUTE, 0)
-        date.set(Calendar.SECOND, 0)
-        date.set(Calendar.MILLISECOND, 0)
-        return Date().time - date.time.time
     }
 }
