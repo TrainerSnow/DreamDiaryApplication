afcad3c7f80b430e4050b1c494670a05f107433b
Simon <simon.bela.bumiller@gmx.de>
2022-11-01 10:36:52 +0100

    Add way to get app context globally

--- app/src/main/java/com/snow/dreamdiary/DreamApp.kt
+++ app/src/main/java/com/snow/dreamdiary/DreamApp.kt
@@ -1,7 +1,23 @@
 package com.snow.dreamdiary
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DreamApp : Application()
+class DreamApp : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        DreamApp.context = this
+    }
+
+    companion object{
+        private var context: Context? = null
+
+        fun getAppContext(): Context{
+            return context ?: throw IllegalStateException("Context is null!")
+        }
+    }
+
+}
