8025061a3810c8bd2e13aaf2977b227df09c3e5c
Simon <simon.bela.bumiller@gmx.de>
2022-11-01 10:29:12 +0100

    Add Top App Bar for OptionsOverviewScreen.kt

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewScreen.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewScreen.kt
@@ -1,9 +1,6 @@
 package com.snow.dreamdiary.feature_dreams.presentation.optionsoverview
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
@@ -11,18 +8,22 @@ import androidx.compose.material.icons.rounded.EditNote
 import androidx.compose.material.icons.rounded.List
 import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material.icons.rounded.Timer
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.snow.dreamdiary.R
+import com.snow.dreamdiary.common.util.TimeFormatUtil
 import com.snow.dreamdiary.feature_dreams.presentation.navigation.DreamScreens
 import com.snow.dreamdiary.feature_dreams.presentation.optionsoverview.components.OptionsItem
 import kotlinx.coroutines.flow.collectLatest
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun OptionsOverviewScreen(
     navController: NavHostController,
@@ -37,63 +38,84 @@ fun OptionsOverviewScreen(
         }
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home)
+                    )
+                },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            )
+        }
     ) {
-
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            verticalArrangement = Arrangement.spacedBy(24.dp),
-            horizontalArrangement = Arrangement.spacedBy(24.dp),
-            contentPadding = PaddingValues(horizontal = 24.dp, vertical = 24.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                    top = it.calculateTopPadding(),
+                    bottom = it.calculateBottomPadding(),
+                    start = it.calculateStartPadding(LayoutDirection.Ltr),
+                    end = it.calculateEndPadding(LayoutDirection.Ltr)
+                )
         ) {
-            item {
-                OptionsItem(
-                    text = stringResource(id = R.string.search_dreams),
-                    icon = Icons.Rounded.Search,
-                    onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.SearchModeScreen)) }
-                )
-            }
 
-            item {
-                OptionsItem(
-                    text = stringResource(id = R.string.daily_survey),
-                    icon = Icons.Rounded.EditNote,
-                    onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.DailySurveyScreen)) }
-                )
-            }
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(24.dp),
+                horizontalArrangement = Arrangement.spacedBy(24.dp),
+                contentPadding = PaddingValues(horizontal = 24.dp, vertical = 24.dp)
+            ) {
+                item {
+                    OptionsItem(
+                        text = stringResource(id = R.string.search_dreams),
+                        icon = Icons.Rounded.Search,
+                        onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.SearchModeScreen)) }
+                    )
+                }
 
-            item {
-                OptionsItem(
-                    text = stringResource(id = R.string.simple_stats),
-                    icon = Icons.Rounded.List,
-                    onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.SimpleStatsScreen)) }
-                )
-            }
+                item {
+                    OptionsItem(
+                        text = stringResource(id = R.string.daily_survey),
+                        icon = Icons.Rounded.EditNote,
+                        onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.DailySurveyScreen)) }
+                    )
+                }
 
-            item {
-                OptionsItem(
-                    text = stringResource(id = R.string.advanced_stats),
-                    icon = Icons.Rounded.List,
-                    onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.AdvancedStatsScreen)) }
-                )
-            }
+                item {
+                    OptionsItem(
+                        text = stringResource(id = R.string.simple_stats),
+                        icon = Icons.Rounded.List,
+                        onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.SimpleStatsScreen)) }
+                    )
+                }
 
-            item {
-                OptionsItem(
-                    text = stringResource(id = R.string.history),
-                    icon = Icons.Rounded.Timer,
-                    onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.HistoryScreen)) }
-                )
-            }
+                item {
+                    OptionsItem(
+                        text = stringResource(id = R.string.advanced_stats),
+                        icon = Icons.Rounded.List,
+                        onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.AdvancedStatsScreen)) }
+                    )
+                }
 
-            items(count = 1, key = null) { i ->
-                OptionsItem(
-                    text = "Item nr $i",
-                    icon = Icons.Rounded.Search,
-                    onClick = { }
-                )
+                item {
+                    OptionsItem(
+                        text = stringResource(id = R.string.history),
+                        icon = Icons.Rounded.Timer,
+                        onClick = { viewModel.onEvent(OptionsOverviewEvent.GoToScreen(DreamScreens.HistoryScreen)) }
+                    )
+                }
+
+                items(count = 1, key = null) { i ->
+                    OptionsItem(
+                        text = "Item nr $i",
+                        icon = Icons.Rounded.Search,
+                        onClick = { }
+                    )
+                }
             }
         }
     }
