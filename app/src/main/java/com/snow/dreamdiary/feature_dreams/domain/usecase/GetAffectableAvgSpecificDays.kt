1475932ed5389c5ec55adc0a8d9aecc963bc608a
Simon <simon.bela.bumiller@gmx.de>
2022-11-06 16:46:35 +0100

    javadoc

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/usecase/GetAffectableAvgSpecificDays.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/usecase/GetAffectableAvgSpecificDays.kt
@@ -4,6 +4,9 @@ import com.snow.dreamdiary.common.util.TimeUtil
 import com.snow.dreamdiary.feature_dreams.domain.model.DailySurveyData
 import com.snow.dreamdiary.feature_dreams.domain.util.DreamAffectables
 
+/**
+ * @return Average value of the affectable (Dream comf. or occurence) on days where selector (e.g. physical activity) was over-averaged
+ */
 public class GetAffectableAvgSpecificDays(
     private val getTimeStampsDailySurveyPartOverAverage: GetTimeStampsDailySurveyPartOverAverage,
     private val getDreamAffectablesAverageUseCase: GetDreamAffectablesAverageUseCase
