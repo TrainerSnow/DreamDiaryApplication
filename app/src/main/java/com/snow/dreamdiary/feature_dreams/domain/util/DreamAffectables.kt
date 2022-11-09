3c5e059c5b2ea18e4e0ebba97ef5d3801bf15933
Simon <simon.bela.bumiller@gmx.de>
2022-11-09 21:04:52 +0100

    Add display name to dream affectables

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamAffectables.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/domain/util/DreamAffectables.kt
@@ -1,6 +1,12 @@
 package com.snow.dreamdiary.feature_dreams.domain.util
 
-sealed class DreamAffectables{
-    object Occurence: DreamAffectables()
-    object Comfortness: DreamAffectables()
+import androidx.annotation.StringRes
+import com.snow.dreamdiary.R
+
+sealed class DreamAffectables(
+    @StringRes val displayNameId: Int
+){
+    object Occurence: DreamAffectables(displayNameId = R.string.dream_occurence)
+    object Comfortness: DreamAffectables(displayNameId = R.string.dream_comfortness)
+
 }
