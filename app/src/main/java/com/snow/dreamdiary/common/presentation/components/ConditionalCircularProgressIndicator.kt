3b3f7a17d76d23ff27b4ad9622da8839d3ac128e
Simon <simon.bela.bumiller@gmx.de>
2022-11-10 12:17:00 +0100

    Make ConditionalCircularProgressIndicator.kt centered

--- app/src/main/java/com/snow/dreamdiary/common/presentation/components/ConditionalCircularProgressIndicator.kt
+++ app/src/main/java/com/snow/dreamdiary/common/presentation/components/ConditionalCircularProgressIndicator.kt
@@ -1,15 +1,21 @@
 package com.snow.dreamdiary.common.presentation.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 
 @Composable
 fun ConditionalCircularProgressIndicator(
     flag: Boolean,
     content: @Composable () -> Unit
 ) {
-    if(flag)
-        content()
-    else
-        CircularProgressIndicator()
+    Box(
+        contentAlignment = Alignment.Center
+    ){
+        if(flag)
+            content()
+        else
+            CircularProgressIndicator()
+    }
 }
