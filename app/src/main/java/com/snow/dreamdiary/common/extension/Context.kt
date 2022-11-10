bf68fdc93551a10ac6a5a9491bae67c11c432e07
Simon <simon.bela.bumiller@gmx.de>
2022-11-10 18:25:15 +0100

    Change hacky function

--- app/src/main/java/com/snow/dreamdiary/common/extension/Context.kt
+++ app/src/main/java/com/snow/dreamdiary/common/extension/Context.kt
@@ -3,6 +3,6 @@ package com.snow.dreamdiary.common.extension;
 import android.content.Context
 import java.io.File
 
-fun Context.databaseFileFromName(name: String): File{
+fun Context.databaseDir(name: String): File{
     return File(this.filesDir.path.removeSuffix("/files").plus("/databases"))
 }
