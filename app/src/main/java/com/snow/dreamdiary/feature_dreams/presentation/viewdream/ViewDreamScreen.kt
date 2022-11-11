7b268eeda952887d7855d6374e0c0fe91a660932
Simon <simon.bela.bumiller@gmx.de>
2022-11-11 19:15:18 +0100

    -

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/viewdream/ViewDreamScreen.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/viewdream/ViewDreamScreen.kt
@@ -1,23 +1,23 @@
 package com.snow.dreamdiary.feature_dreams.presentation.viewdream
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Scaffold
-import androidx.compose.material.SnackbarResult
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Edit
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -30,13 +30,15 @@ import com.snow.dreamdiary.feature_dreams.presentation.viewdream.components.Modi
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ViewDreamScreen(
     bottomNavController: NavHostController,
     viewModel: ViewDreamViewModel = hiltViewModel()
 ) {
 
-    val scaffoldState = rememberScaffoldState()
+    val snackbarHostState = remember { SnackbarHostState() }
+
     val scope = rememberCoroutineScope()
 
     val undoMsg = stringResource(id = R.string.undo)
@@ -53,75 +55,76 @@ fun ViewDreamScreen(
     }
 
     Scaffold(
-        scaffoldState = scaffoldState
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) {
         val ignored = it
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = it.calculateTopPadding() + 36.dp)
         ) {
             ConditionalCircularProgressIndicator(
                 flag = viewModel.state.value.dream != null
             ) {
-                val dream = viewModel.state.value.dream!!
+                Column(){
+                    val dream = viewModel.state.value.dream!!
 
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    text = "${stringResource(id = R.string.dream_from)} ${
-                        TimeFormatUtil.getMillisDayFormatted(
-                            dream.dreamtAt
-                        )
-                    }",
-                    style = MaterialTheme.typography.displaySmall,
-                    textAlign = TextAlign.Center
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceAround
-                ) {
-                    OutlinedButton(onClick = {
-                        viewModel.onEvent(ViewDreamEvent.Delete)
-
-                        scope.launch {
-                            val result = scaffoldState.snackbarHostState.showSnackbar(
-                                message = deletedMsg,
-                                actionLabel = undoMsg
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = "${stringResource(id = R.string.dream_from)} ${
+                            TimeFormatUtil.getMillisDayFormatted(
+                                dream.dreamtAt
                             )
-                            if(result == SnackbarResult.ActionPerformed){
-                                viewModel.onEvent(ViewDreamEvent.Restore)
-                            }
+                        }",
+                        style = MaterialTheme.typography.displaySmall,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(12.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        OutlinedButton(
+                            onClick = {
+                                viewModel.onEvent(ViewDreamEvent.Delete)
+
+                                scope.launch {
+                                    val result = snackbarHostState.showSnackbar(
+                                        message = deletedMsg,
+                                        actionLabel = undoMsg
+                                    )
+                                    if (result == SnackbarResult.ActionPerformed) {
+                                        viewModel.onEvent(ViewDreamEvent.Restore)
+                                    }
+                                }
+                            }) {
+                            Icon(
+                                imageVector = Icons.Rounded.Delete,
+                                contentDescription = stringResource(id = R.string.delete)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = stringResource(id = R.string.delete))
                         }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Delete,
-                            contentDescription = stringResource(id = R.string.delete)
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(text = stringResource(id = R.string.delete))
+                        Button(onClick = { viewModel.onEvent(ViewDreamEvent.Edit) }) {
+                            Icon(
+                                imageVector = Icons.Rounded.Edit,
+                                contentDescription = stringResource(id = R.string.edit)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = stringResource(id = R.string.edit))
+                        }
                     }
-                    Button(onClick = { viewModel.onEvent(ViewDreamEvent.Edit) }) {
-                        Icon(
-                            imageVector = Icons.Rounded.Edit,
-                            contentDescription = stringResource(id = R.string.edit)
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Text(text = stringResource(id = R.string.edit))
-                    }
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                ) {
+                    Spacer(modifier = Modifier.height(12.dp))
                     Column(
                         modifier = Modifier
                             .clip(shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp))
-                            .fillMaxSize()
+                            .fillMaxWidth()
                             .background(MaterialTheme.colorScheme.secondaryContainer)
                             .verticalScroll(rememberScrollState())
                             .padding(16.dp)
+                            .weight(1F)
                     ) {
                         ModifierDescriptionItem(
                             title = stringResource(id = R.string.description),
