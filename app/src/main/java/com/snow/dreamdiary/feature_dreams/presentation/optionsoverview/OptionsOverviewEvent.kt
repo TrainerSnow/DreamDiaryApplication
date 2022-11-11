133e54297d8f4a4b14af2f9065fe4ea71a5cabe1
Simon <simon.bela.bumiller@gmx.de>
2022-11-11 08:19:40 +0100

    Change event signatu re, need data to be sent

--- app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewEvent.kt
+++ app/src/main/java/com/snow/dreamdiary/feature_dreams/presentation/optionsoverview/OptionsOverviewEvent.kt
@@ -1,10 +1,11 @@
 package com.snow.dreamdiary.feature_dreams.presentation.optionsoverview
 
+import android.net.Uri
 import com.snow.dreamdiary.feature_dreams.presentation.navigation.DreamScreens
 
 sealed class OptionsOverviewEvent {
     data class GoToScreen(val screen: DreamScreens) : OptionsOverviewEvent()
 
     object Backup: OptionsOverviewEvent()
-    object RestoreBackup: OptionsOverviewEvent()
+    data class RestoreBackup(val data: Uri): OptionsOverviewEvent()
 }
